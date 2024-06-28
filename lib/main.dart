79ffefd6c7a0eb3304d36c5226efa32557e68156
alisonlai <mr.lai.who@gmail.com>
2024-06-28 15:19:00 +0800

    feat: 🎸 跳转目录

--- lib/main.dart
+++ lib/main.dart
@@ -1,178 +1,176 @@
 import 'package:comic_box/view/storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'common/toast.dart';
 import 'view/directory.dart'; // 导入 directory.dart 文件
 
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+Future<void> main() async {
+  runApp(const MyApp());
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
-  late TabController _tabController;
-  List<String> _directoryContents = []; // 存储目录内容
-  late DirectoryLoader _directoryLoader; // 声明_directoryLoader字段
-  bool _directoryLoaded = false; // 标志是否已加载目录内容
-
-  @override
-  void initState() {
-    if (kDebugMode) {
-      print("===========main initState init");
-    }
-    super.initState();
-    _tabController = TabController(length: 4, vsync: this);
-    _directoryLoader = DirectoryLoader(config: serverConfigs[0]); // 初始化_directoryLoader字段
-    // _tabController.addListener(_handleTabSelection);
-  }
-
-  void _handleTabSelection() {
-    if (_tabController.index == 1 && !_directoryLoaded) {
-      _directoryLoader = DirectoryLoader(config: serverConfigs[0]);
-      _loadDirectory();
-    }
-  }
-
-  // 加载目录内容
-  Future<void> _loadDirectory() async {
-    try {
-      final List<String> directoryContents = await _directoryLoader.loadDirectory();
-      if (kDebugMode) {
-        print("==============directoryContents:$_directoryContents");
-      }
-      setState(() {
-        _directoryContents = directoryContents;
-        _directoryLoaded = true;
-      });
-    } catch (e, stackTrace) {
-      if (kDebugMode) {
-        print("Error occurred in _loadDirectory: $e\n$stackTrace");
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return Scaffold(
-        appBar: AppBar(
-        title: Text(widget.title),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const [
-              Tab(text: '源'),
-              Tab(text: '目录'),
-              Tab(text: '最近观看'),
-              Tab(text: '书签'),
-            ],
-            onTap: (index) {
-              if (index == 1 && !_directoryLoaded) { // 点击了第二个标签页且目录尚未加载
-                _loadDirectory();
-              }
-            },
-          ),
-        ),
-    body: TabBarView(
-      controller: _tabController,
-      children: [
-        ListView.builder (
-          itemCount: serverConfigs.length,
-          itemBuilder: (context, index) {
-            final config = serverConfigs[index];
-            return ListTile(
-              title: Text(config.title),
-              onTap: () {
-                // 处理点击事件，例如导航到特定页面或执行其他操作
-                if (kDebugMode) {
-                  print('Clicked on ${config.host}');
-                }
-              },
-            );
-          },
-        ),
-        // 第二个标签页的内容
-        _directoryContents.isEmpty
-            ? const Center(child: CircularProgressIndicator()) // 加载中
-            : ListView.builder(
-          itemCount: _directoryContents.length,
-          itemBuilder: (context, index) {
-            return ListTile(title: Text(_directoryContents[index]));
-          },
-        ),
-        // 第二个Tab对应的页面
-        // FutureBuilder(
-        //   future: _loadDirectory(), // 执行加载目录内容的方法
-        //   builder: (context, snapshot) {
-        //     if (snapshot.connectionState == ConnectionState.waiting) {
-        //       // 加载中，显示加载指示器
-        //       return const Center(child: CircularProgressIndicator());
-        //     } else if (snapshot.hasError) {
-        //       // 加载出错，显示错误信息
-        //       if (kDebugMode) {
-        //         print("Error: ${snapshot.error}");
-        //       }
-        //       return Center(child: Text('Error: ${snapshot.error}'));
-        //     } else {
-        //       // 加载完成，显示目录内容
-        //       return ListView.builder(
-        //         itemCount: _directoryContents.length,
-        //         itemBuilder: (context, index) {
-        //           return GestureDetector(
-        //             onTap: () {
-        //               // 处理点击事件，例如导航到特定页面或执行其他操作
-        //               if (kDebugMode) {
-        //                 print('Clicked on ${_directoryContents[index]}');
-        //               }
-        //             },
-        //             child: ListTile(
-        //               title: Text(_directoryContents[index]),
-        //             ),
-        //           );
-        //         },
-        //       );
-        //     }
-        //   },
-        // ),
-        const Center(child: Text('最近观看内容')),
-        const Center(child: Text('书签内容')),
-        ],
-      ),
-      bottomNavigationBar: kDebugMode ? Text("Total: ${_directoryContents.length}") : null,
-    );
-      }
-
-    @override
-    void dispose() {
-    _tabController.dispose();
-    super.dispose();
-    }
-
-    @override
-    bool get wantKeepAlive => true;
-  }
-
-
-  Future<void> main() async {
-    runApp(const MyApp());
-  }
-
-  class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-  title: '漫画箱',
-  theme: ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-  useMaterial3: true,
-  ),
-  home: const MyHomePage(title: '漫画箱'),
-  );
+    return MaterialApp(
+      title: '漫画箱',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: '漫画箱'),
+    );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  List<String> _directoryContents = [];
+  DirectoryLoader? _directoryLoader;
+  bool _directoryLoaded = false;
+  String _currentDirectory = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.addListener(() {
+      if (_tabController.index == 1 && !_directoryLoaded && _directoryLoader != null) {
+        _loadDirectory();
+      }
+    });
   }
+
+  Future<void> _loadDirectory([String directoryPath = '']) async {
+    try {
+      List<String> contents = await _directoryLoader!.loadDirectory(directoryPath);
+      setState(() {
+        _directoryContents = contents;
+        _directoryLoaded = true;
+        _currentDirectory = directoryPath;
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error occurred in _loadDirectory: $e");
+      }
+      alterShowToast("Error: $e");
+    }
+  }
+
+  void _onServerTap(ServerConfig config) {
+    setState(() {
+      _directoryLoader = DirectoryLoader(config: config);
+      _directoryLoaded = false;
+      _currentDirectory = '';
+    });
+    _loadDirectory();
+    _tabController.animateTo(1); // 切换到第二个标签页
+  }
+
+  void _navigateUp() {
+    if (_currentDirectory.isNotEmpty && _currentDirectory.contains('/')) {
+      // 移除最后一个 '/' 后的部分，跳转到上层目录
+      String parentDirectory = _currentDirectory.substring(0, _currentDirectory.lastIndexOf('/'));
+      _loadDirectory(parentDirectory);
+    } else {
+      // 如果已经是根目录，重置为根目录
+      _loadDirectory('');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: '源'),
+            Tab(text: '目录'),
+            Tab(text: '最近观看'),
+            Tab(text: '书签'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ListView.builder(
+            itemCount: serverConfigs.length,
+            itemBuilder: (context, index) {
+              final config = serverConfigs[index];
+              return ListTile(
+                title: Text(config.title),
+                subtitle: Text('Port: ${config.port} - User: ${config.user}'),
+                onTap: () => _onServerTap(config), // 调用 _onServerTap 方法
+              );
+            },
+          ),
+          FutureBuilder<List<String>>(
+            future: _directoryLoader?.loadDirectory(_currentDirectory), // 调用 _directoryLoader 加载目录
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                if (kDebugMode) {
+                  print("Error: ${snapshot.error}");
+                }
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                _directoryContents = snapshot.data ?? [];
+                return Column(
+                  children: [
+                    // 固定导航栏
+                    ListTile(
+                      leading: const Icon(Icons.arrow_upward),
+                      title: const Text('上层目录'),
+                      onTap: _currentDirectory.isNotEmpty ? _navigateUp : null,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _directoryContents.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              String selectedDirectory = _directoryContents[index];
+                              if (kDebugMode) {
+                                print('Clicked on $selectedDirectory');
+                              }
+                              String newDirectory = _currentDirectory.isEmpty
+                                  ? selectedDirectory
+                                  : '$_currentDirectory/$selectedDirectory';
+                              _loadDirectory(newDirectory); // 进入点击的目录
+                            },
+                            child: ListTile(
+                              title: Text(_directoryContents[index]),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              }
+            },
+          ),
+          const Center(child: Text('最近观看内容')),
+          const Center(child: Text('书签内容')),
+        ],
+      ),
+      bottomNavigationBar: kDebugMode ? Text("Total: ${_directoryContents.length}") : null,
+    );
+  }
+}
+
--- lib/view/directory.dart
+++ lib/view/directory.dart
@@ -6,28 +6,22 @@ import 'package:flutter/foundation.dart';
 import 'package:comic_box/common/toast.dart';
 import 'package:comic_box/ftp/ftpconnect.dart';
 
-class DirectoryLoader extends StatelessWidget {
+class DirectoryLoader {
   final ServerConfig config;
-  late final FTPConnect _ftpClient;
-  late final String _address;
-  DirectoryLoader({super.key, required this.config});
+  late FTPConnect _ftpClient;
+  String? _address;
 
-
-  Future<List<String>> loadDirectory() async {
-    // 连接到FTP服务器
+  DirectoryLoader({required this.config}) {
     _ftpClient = FTPConnect(config.host, port: config.port, user: config.user, pass: config.pass);
-    await _ftpClient.connect();
-    if (kDebugMode) {
-      print("==========loadDirectory step 1.");
-    }
-    List<FTPEntry> listDir = await _ftpClient.listDirectoryContent();
+  }
 
+  Future<List<String>> loadDirectory([String directoryPath = '']) async {
     List<String> filePaths = [];
-    // 将域名解析为 IP 地址
-    alterShowToast("loadDirectory begin lookup ${config.host}");
+
     try {
-      List<InternetAddress> addresses = await InternetAddress.lookup(config.host,
-          type: InternetAddressType.any);
+      // 将域名解析为 IP 地址
+      alterShowToast("loadDirectory begin lookup ${config.host}");
+      List<InternetAddress> addresses = await InternetAddress.lookup(config.host, type: InternetAddressType.any);
       alterShowToast("InternetAddress baidu: $addresses");
       if (addresses.isEmpty) {
         alterShowToast("${config.host} lookup fail");
@@ -38,6 +32,26 @@ class DirectoryLoader extends StatelessWidget {
       }
       InternetAddress address = addresses.first;
       _address = address.address;
+
+      // 连接到FTP服务器，使用解析到的IP地址
+      _ftpClient = FTPConnect(_address!, port: config.port, user: config.user, pass: config.pass);
+      await _ftpClient.connect();
+      if (kDebugMode) {
+        print("==========loadDirectory step 2.");
+      }
+
+      // 切换到指定目录
+      if (directoryPath.isNotEmpty) {
+        await _ftpClient.changeDirectory(directoryPath);
+      }
+
+      List<FTPEntry> listDir = await _ftpClient.listDirectoryContent();
+
+      for (FTPEntry dir in listDir) {
+        filePaths.add(dir.name);
+      }
+
+      return filePaths;
     } catch (e) {
       if (kDebugMode) {
         print(e.toString());
@@ -45,99 +59,5 @@ class DirectoryLoader extends StatelessWidget {
       alterShowToast(e.toString());
       return [];
     }
-    // 连接到FTP服务器
-    _ftpClient = FTPConnect(_address, port: config.port, user: config.user, pass: config.pass);
-    try {
-      bool connected = await _ftpClient.connect();
-      if (kDebugMode) {
-        print("==========loadDirectory step 1.");
-      }
-      List<FTPEntry> listDir = await _ftpClient.listDirectoryContent();
-
-      for (FTPEntry dir in listDir) {
-        filePaths.add(dir.name);
-      }
-    } catch (e) {
-      alterShowToast(e.toString());
-      return [];
-    }
-
-    return filePaths;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        // 处理点击事件，例如导航到特定页面或执行其他操作
-        if (kDebugMode) {
-          print('Clicked on index');
-        }
-      },
-      child: ListTile(
-        title: Text('test'),
-      ),
-    );
   }
 }
-
-
-// class DirectoryLoader {
-//   late final String _host;
-//   late final int _port;
-//   late final String _user;
-//   late final String _pass;
-//   late final FTPConnect _ftpClient;
-//   late final String _address;
-//   DirectoryLoader(String host, int port, String user, String pass):
-//    _host = host, _port = port, _user = user, _pass = pass {
-//     if (kDebugMode) {
-//       print("===========DirectoryLoader init");
-//     }
-//   }
-//   StreamSubscription? _subscription;
-//
-//   Future<List<String>> loadDirectory(String directoryPath) async {
-//     List<String> filePaths = [];
-//     // 将域名解析为 IP 地址
-//     alterShowToast("loadDirectory begin lookup $_host");
-//     try {
-//       List<InternetAddress> addresses = await InternetAddress.lookup(_host,
-//           type: InternetAddressType.any);
-//       alterShowToast("InternetAddress baidu: $addresses");
-//       if (addresses.isEmpty) {
-//         alterShowToast("$_host lookup fail");
-//         if (kDebugMode) {
-//           print("$_host lookup fail");
-//         }
-//         throw Exception('Failed to resolve host: $_host');
-//       }
-//       InternetAddress address = addresses.first;
-//       _address = address.address;
-//     } catch (e) {
-//       if (kDebugMode) {
-//         print(e.toString());
-//       }
-//       alterShowToast(e.toString());
-//       return [];
-//     }
-//     // 连接到FTP服务器
-//     _ftpClient = FTPConnect(_address, port: _port, user: _user, pass: _pass);
-//     try {
-//       bool connected = await _ftpClient.connect();
-//       if (kDebugMode) {
-//         print("==========loadDirectory step 1.");
-//       }
-//       List<FTPEntry> listDir = await _ftpClient.listDirectoryContent();
-//
-//       for (FTPEntry dir in listDir) {
-//         filePaths.add(dir.name);
-//       }
-//     } catch (e) {
-//       alterShowToast(e.toString());
-//       return [];
-//     }
-//
-//     return filePaths;
-//   }
-// }
